df690ffd8a470c4f0fe82dd81f8830e894c44546
Dhouglas Saraiva <48390877+dhougsaraiva@users.noreply.github.com>
2019-04-18 22:21:53 -0300

    Classes atualizadas

--- ProjetoPS2/src/mack/projeto/ps2/ContaBancaria.java
+++ ProjetoPS2/src/mack/projeto/ps2/ContaBancaria.java
@@ -1,5 +1,50 @@
 package mack.projeto.ps2;
 
 public class ContaBancaria {
+    private long idContaBancaria;
+    private String nomeCliente;
+    private String saldo;
+    private int numeroAgencia;
+    
+    public ContaBancaria(){ }
+    
+    public ContaBancaria(long idTime,String nome,String saldo, int numeroAgencia){
+        this.idContaBancaria = idContaBancaria;
+        this.nomeCliente = nomeCliente;
+        this.saldo = saldo;
+        this.numeroAgencia = numeroAgencia;
+    }
+
+    public long getIdContaBancaria() {
+        return idContaBancaria;
+    }
+
+    public void setIdContaBancaria(long idContaBancaria) {
+        this.idContaBancaria = idContaBancaria;
+    }
+
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+
+    public void setNomeCliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+
+    public String getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(String saldo) {
+        this.saldo = saldo;
+    }
+
+    public int getNumeroAgencia() {
+        return numeroAgencia;
+    }
+
+    public void setNumeroAgencia(int numeroAgencia) {
+        this.numeroAgencia = numeroAgencia;
+    }
     
 }
--- ProjetoPS2/src/mack/projeto/ps2/Jogo.java
+++ ProjetoPS2/src/mack/projeto/ps2/Jogo.java
@@ -1,5 +1,60 @@
 package mack.projeto.ps2;
 
 public class Jogo {
+    private long idJogo;
+    private String nomeTimeA;
+    private String nomeTimeB;
+    private int golsTimeA;
+    private int golsTimeB;
     
+    public Jogo(){ }
+    
+
+    public Jogo(long idJogo,String nomeTimeA,String nomeTimeB, int golsTimeA,  int golsTimeB){
+        this.idJogo = idJogo;
+        this.nomeTimeA = nomeTimeA;
+        this.nomeTimeB = nomeTimeB;
+        this.golsTimeA = golsTimeA;
+        this.golsTimeB = golsTimeB;
+    }
+
+    public long getIdJogo() {
+        return idJogo;
+    }
+
+    public void setIdJogo(long idJogo) {
+        this.idJogo = idJogo;
+    }
+
+    public String getNomeTimeA() {
+        return nomeTimeA;
+    }
+
+    public void setNomeTimeA(String nomeTimeA) {
+        this.nomeTimeA = nomeTimeA;
+    }
+
+    public String getNomeTimeB() {
+        return nomeTimeB;
+    }
+
+    public void setNomeTimeB(String nomeTimeB) {
+        this.nomeTimeB = nomeTimeB;
+    }
+
+    public int getGolsTimeA() {
+        return golsTimeA;
+    }
+
+    public void setGolsTimeA(int golsTimeA) {
+        this.golsTimeA = golsTimeA;
+    }
+
+    public int getGolsTimeB() {
+        return golsTimeB;
+    }
+
+    public void setGolsTimeB(int golsTimeB) {
+        this.golsTimeB = golsTimeB;
+    }
 }
