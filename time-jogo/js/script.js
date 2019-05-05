a4e667a729ae88a5e9c40b6c144996d96a7d39d3
vinicastelani <48360255+vinicastelani@users.noreply.github.com>
2019-05-05 00:05:31 -0300

    UPDATED INTERFACE

--- time-jogo/js/script.js
+++ time-jogo/js/script.js
@@ -1,17 +1,84 @@
-$(document).ready(function(){
-  var ballLeft = ("#ball-left");
-  var ballRight = ("#ball-right");
+$(window).ready(function() {
 
-  
-  $(ballLeft).hover(function(){
-    $(ballLeft).animate({top: "-10"});
-    $(ballLeft).animate({top: "0"});
+  var status = $("#status");
+  $(status).html("");
+  $(status).hide();
+  // START PARDIDA
+  $("#start").on("click", function() {
+    $("#placar-1").html("0");
+    $("#placar-2").html("0");
+    ataque();
   });
+  // START PARDIDA
 
-  $(ballRight).hover(function(){
-    $(ballRight).animate({top: "-10"});
-    $(ballRight).animate({top: "0"});
-  });
+  // PLACAR
+  var scoreA = 0;
+  var scoreB = 0;
+  var fimDeJogo = false;
+  // PLACAR
+  // RODADA DE ATAQUE
+  function ataque() {
+    var turn = Math.floor((Math.random() * 100) + 1);
+    console.log(turn);
+    if (turn % 2 == 0) {
+      // SE PAR ESTIVER ATACANDO
+      $(status).show();
+      $(status).html("PAR ESTA ATACANDO");
+      setTimeout(function() {
+        var chance = Math.floor((Math.random() * 100) + 1);
+        if (chance % 2 == 0) {
+          $(status).html("PAR FEZ O GOL !");
+          scoreA ++;
+          $("#placar-1").html(scoreA);
+          setTimeout(function() {
+            $(status).hide()
+          }, 3000);
+        } else {
+          $(status).html("PAR PERDEU O GOL !");
+          setTimeout(function() {
+            $(status).hide()
+          }, 3000);
+        }
+      }, 3000);
+      // SE PAR ESTIVER ATACANDO
+    }
+    // SE IMPAR ESTIVER ATACANDO
+    else {
+      $(status).show();
+      $(status).html("IMPAR ESTA ATACANDO");
+      setTimeout(function() {
+        var chance = Math.floor((Math.random() * 100) + 1);
+        if (chance % 2 == 0) {
+          $(status).html("IMPAR FEZ O GOL !");
+          scoreB ++;
+          $("#placar-2").html(scoreB);
+          setTimeout(function() {
+            $(status).hide()
+          }, 3000);
+        } else {
+          $(status).html("IMPAR PERDEU O GOL !");
+          setTimeout(function() {
+            $(status).hide()
+          }, 3000);
+        }
+      }, 3000);
+    }
+    if(scoreA == 3 || scoreB == 3){
+      fimDeJogo = true;
+    } else{
+      fimDeJogo = false;
+    }
+    if(fimDeJogo == false)
+    {
+      setTimeout(function(){
+        ataque();
+      },7000);
+    }
+    console.log("PLACAR "+scoreA);
+    console.log("PLACAR "+scoreB);
+  }
 
+  // SE IMPAR ESTIVER ATACANDO
+  // RODADA DE ATAQUE
 
 });
